97c768e0ab53181c5e88f69f1ef97fb64a779e3e
patilaniruddha98 <66047669+patilaniruddha98@users.noreply.github.com>
2023-01-12 23:10:42 +0530

    ec2.tf-1

--- ec2.tf
+++ ec2.tf
@@ -9,7 +9,7 @@ module "ec2_instance" {
   key_name               = "upgrad"
   monitoring             = true
   vpc_security_group_ids = [module.test_service_sg.security_group_id]
-  subnet_id              = [module.vpc.public_subnets]
+  subnet_id              = module.vpc.public_subnets[0]
 
   tags = {
     Terraform   = "true"
