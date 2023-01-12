d64d22d94b4a41620cdc4889b27178d3219a0a54
patilaniruddha98 <66047669+patilaniruddha98@users.noreply.github.com>
2023-01-12 22:43:30 +0530

    sg.tf

---
+++ sg.tf
@@ -0,0 +1,17 @@
+module "test_service_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "test-sg-service"
+  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  vpc_id      = module.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+       from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "ssh"
+      cidr_blocks = "0.0.0.0/0" 
+    }
+  ]
+}
