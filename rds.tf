172137de124b1aac2231ccfb358f9518707bc266
patilaniruddha98 <66047669+patilaniruddha98@users.noreply.github.com>
2023-01-12 23:37:06 +0530

    rds.tf-3

--- rds.tf
+++ rds.tf
@@ -5,7 +5,7 @@ module "db" {
 
   engine            = "mysql"
   engine_version    = "5.7"
-  instance_class    = "db.t3a.large"
+  instance_class    = "db.t4g.micro"
   allocated_storage = 5
 
   db_name  = "demodb"
